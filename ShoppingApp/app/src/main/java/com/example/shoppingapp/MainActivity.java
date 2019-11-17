1a5c167a0ec6ab6aa9d602052081904266ef8052
MJedr <jedrych.marcysia@gmail.com>
2019-11-17 14:33:57 +0100

    changing navbar on main activity works

--- ShoppingApp/app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ ShoppingApp/app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -8,8 +8,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -19,13 +22,17 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,36 +56,42 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences mSharedPreferences;
     private Context mContext;
     TextView sizetxt;
+    public static final String KEY_THEME_PREFERENCE = "themePref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mContext = getApplicationContext();
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-
-        RelativeLayout rl = (RelativeLayout)findViewById(R.id.activity_main);
-        String backgroundColor = mSharedPreferences.getString(getString(R.string.background_color),"#FFFFFF");
-        rl.setBackgroundColor(Color.parseColor(backgroundColor));
-
-        Configuration configuration = getResources().getConfiguration();
-        float txtSize = Float.parseFloat(mSharedPreferences.
-                getString(getString(R.string.text_size), "1.0"));
-        configuration.fontScale *= txtSize;
-        getResources().updateConfiguration(configuration, getResources().getDisplayMetrics());
 
         mHelper = new ItemHelper(this);
-        mItemListView = (ListView) findViewById(R.id.list_to_buy);
+        mContext = getApplicationContext();
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        String appTheme = mSharedPreferences.getString("theme_prefer", "AppTheme");
+        if (appTheme.equals("AppTheme")) {
+            setTheme(R.style.AppTheme);
+        }else{
+            setTheme(R.style.AppThemeCustom);
+        }
+        setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.add_to_list_btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog(MainActivity.this);
-            }
-        });
-        updateUI();
-    }
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
+        String backgroundColor = mSharedPreferences.getString(getString(R.string.background_color), "#FFFFFF");
+        rl.setBackgroundColor(Color.parseColor(backgroundColor));
+
+        TextView nb_of_items_view = (TextView) findViewById(R.id.nb_of_it);
+        TextView descr2 = (TextView) findViewById(R.id.nb_of_items_descr2);
+        Long nb_of_items = queryNumberItems();
+        String info_nb_of_items;
+        if (nb_of_items == 0) {
+            info_nb_of_items = "Hello!\n At your list there are no items to buy!";
+        } else if (nb_of_items == 1) {
+            info_nb_of_items = "item to buy";
+        } else {
+            info_nb_of_items = "items to buy";
+        }
+
+        descr2.setText(info_nb_of_items);
+        nb_of_items_view.setText(Long.toString(nb_of_items));
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -94,125 +107,19 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
             return true;
-        }
+        }else if (id == R.id.create_list) {
+                Intent intent = new Intent(MainActivity.this, ProductListActivity.class);
+                startActivity(intent);
+                return true;
+            }
         return super.onOptionsItemSelected(item);
     }
 
-    private void setColor(boolean background) {
-        RelativeLayout rl = (RelativeLayout)findViewById(R.id.activity_main);
-        if (background == true) {
-            rl.setBackgroundColor(Color.WHITE);
-        } else {
-            rl.setBackgroundColor(Color.YELLOW);
-        }
-    }
 
-    private void updateUI() {
-        final ArrayList<GroceryItem> groceryItemList = new ArrayList<>();
+    public long queryNumberItems(){
         SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(Item.ItemEntry.TABLE,
-                new String[]{Item.ItemEntry._ID, Item.ItemEntry.COL_ITEM_NAME,
-                        Item.ItemEntry.COL_AMOUNT, Item.ItemEntry.COL_UNIT_PRICE,
-                        Item.ItemEntry.COL_UNITS, Item.ItemEntry.COL_CURRENCY},
-                null, null, null, null, null, null);
-
-        while (cursor.moveToNext()) {
-            int index = cursor.getColumnIndex(Item.ItemEntry._ID);
-            GroceryItem groceryItem;
-            String name = cursor.getString(cursor.getColumnIndex("name"));
-            String amount = cursor.getString(cursor.getColumnIndex("amount"));
-            String unit_price = cursor.getString(cursor.getColumnIndex("unit_price"));
-            String units = cursor.getString(cursor.getColumnIndex("units"));
-            String currency = cursor.getString(cursor.getColumnIndex("currency"));
-            groceryItemList.add(new GroceryItem(name, amount, unit_price, units, currency));
-//                itemList.add(cursor.getString(index));
-
-        }
-
-        if (mAdapter == null) {
-            if(groceryItemList.isEmpty()){
-
-            }
-            else{
-                mAdapter = new GroceryItemArrayAdapter(this,groceryItemList);
-                mItemListView.setAdapter(mAdapter);
-            }
-        }
-            else {
-                mAdapter.clear();
-                mAdapter.addAll(groceryItemList);
-                mAdapter.notifyDataSetChanged();
-            }
-
-            cursor.close();
-            db.close();
-        }
-
-    public void deleteItem(View view){
-        View parent = (View) view.getParent();
-        TextView itemTextView = (TextView) parent.findViewById(R.id.item_title);
-        String item = String.valueOf(itemTextView.getText());
-        SQLiteDatabase db = mHelper.getWritableDatabase();
-        db.delete(Item.ItemEntry.TABLE, Item.ItemEntry.COL_ITEM_NAME + " = ?", new String[] {item});
+        long count = DatabaseUtils.queryNumEntries(db, Item.ItemEntry.TABLE);
         db.close();
-        updateUI();
-    }
-
-
-    public void showDialog(Activity activity) {
-        final Dialog dialog = new Dialog(activity);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setCancelable(false);
-        dialog.setContentView(R.layout.add_item_dialog);
-
-        final EditText itemInput = dialog.findViewById(R.id.item_name);
-        final EditText amountInput = dialog.findViewById(R.id.amount);
-        final EditText unitPriceInput = dialog.findViewById(R.id.unit_price);
-        final EditText unitsInput = dialog.findViewById(R.id.units);
-        final EditText currencyInput = dialog.findViewById(R.id.currency);
-
-        Button btnok = (Button) dialog.findViewById(R.id.btnok);
-
-        btnok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String item = itemInput.getText().toString();
-                String amount = amountInput.getText().toString();
-                String unitPrice = unitPriceInput.getText().toString();
-                String units = unitsInput.getText().toString();
-                String currency = currencyInput.getText().toString();
-
-                SQLiteDatabase db = mHelper.getWritableDatabase();
-//                mHelper.onUpgrade(db, 5, 6);
-
-                ContentValues values = new ContentValues();
-                values.put(Item.ItemEntry.COL_ITEM_NAME, item);
-                values.put(Item.ItemEntry.COL_AMOUNT, amount);
-                values.put(Item.ItemEntry.COL_UNIT_PRICE, unitPrice);
-                values.put(Item.ItemEntry.COL_UNITS, units);
-                values.put(Item.ItemEntry.COL_CURRENCY, currency);
-                db.insertWithOnConflict(Item.ItemEntry.TABLE,
-                        null, values, SQLiteDatabase.CONFLICT_REPLACE);
-                db.close();
-
-                dialog.dismiss();
-
-                updateUI();
-            }
-        });
-
-        Button btncn = (Button) dialog.findViewById(R.id.btncn);
-        btncn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-    }
-    // Method to set Size of Text.
-    private void changeTextSize(Float i) {
-        sizetxt.setTextSize(i);
+        return count;
     }
 }
